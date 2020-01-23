ec1805df75def2f91290729ba68d1b50b1f3ad06
Pratimkataria520 <katariap@sheridancollege.ca>
2020-01-22 23:07:08 -0500

    Reverted

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -17,17 +17,7 @@ public class Student {
     private String email;
     private String mobileModel;
     private int number;
-    private int ut;
 
-    public int getUt() {
-        return ut;
-    }
-
-    public void setUt(int ut) {
-        this.ut = ut;
-    }
-
-    
     public int getNumber() {
         return number;
     }
@@ -36,6 +26,7 @@ public class Student {
         this.number = number;
     }
     
+
     public String getEmail() {
         return email;
     }
